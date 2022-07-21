9baf36d57f19db9029f864b93827300f5cff127d
BakayAlexander <bakay.dvr@gmail.com>
2022-07-21 15:40:24 +0300

    feat: add Portfolio logic

--- components/UI/Portfolio/Portfolio.jsx
+++ components/UI/Portfolio/Portfolio.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { Container, Row, Col } from 'reactstrap';
+import { categoryProjects } from '../../../constants/constants';
 import { portfolioData } from '../../data/portfolio';
 import PortfolioItem from '../PortfolioItem/PortfolioItem';
 import SectionSubtitle from '../SectionSubtitle/SectionSubtitle';
 import classes from './portfolio.module.css';
 
 const Portfolio = () => {
+  const [filter, setFilter] = useState('All');
+  const [data, setData] = useState(portfolioData);
+  const activeButtonClass = `${classes.tab__btn_active}`;
+
+  const sortReverse = arr => arr.sort((a, b) => b.id - a.id);
+
+  const updateData = useCallback(filter => {
+    if (filter === categoryProjects.all) {
+      return setData(sortReverse(portfolioData));
+    }
+    const filteredData = portfolioData.filter(item => item.category === filter);
+    setData(sortReverse(filteredData));
+  }, []);
+
+  useEffect(() => {
+    updateData(filter);
+  }, [filter, updateData]);
+
   return (
     <section id="portfolio">
       <Container>
@@ -16,11 +35,27 @@ const Portfolio = () => {
           </Col>
           <Col lg="6" md="6">
             <div className="text-end">
-              <button className={`${classes.tab__btn_active} secondary__btn`}>Commercial projects</button>
-              <button className={`${classes.tab__btn_active} secondary__btn text-white`}>Study projects</button>
+              <button
+                className={`${filter === categoryProjects.all && activeButtonClass} secondary__btn text-white`}
+                onClick={() => setFilter(categoryProjects.all)}
+              >
+                {categoryProjects.all}
+              </button>
+              <button
+                className={`${filter === categoryProjects.commercial && activeButtonClass} secondary__btn  text-white`}
+                onClick={() => setFilter(categoryProjects.commercial)}
+              >
+                {categoryProjects.commercial}
+              </button>
+              <button
+                className={`${filter === categoryProjects.study && activeButtonClass} secondary__btn text-white`}
+                onClick={() => setFilter(categoryProjects.study)}
+              >
+                {categoryProjects.study}
+              </button>
             </div>
           </Col>
-          {portfolioData.map(portfolio => (
+          {data?.map(portfolio => (
             <Col lg="4" md="4" key={portfolio.id}>
               <PortfolioItem
                 title={portfolio.title}
